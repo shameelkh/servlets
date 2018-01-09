8abe9dbbf922c3e9af58d1b5d3c01fc2d608e1ae
shameelkh <shameel-khan@hotmail.com>
2018-01-09 07:12:13 -0500

    Add LoginServlet and random.jsp

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        String url = "/random.jsp";
+        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(url);
+        requestDispatcher.forward(request, response);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>SimpleServlet</title>
+</head>
+<body>
+    <h1>Welcome to SimpleServlet</h1>
+
+    <form action="login" method="POST">
+        Username: <input type="text" /> <br/>
+        Password: <input type="password" /> <br />
+        <input type="submit" value="Log In"/>
+    </form>
+</body>
+</html>
